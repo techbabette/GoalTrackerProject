b62d7edc1e4321d9508b6ab2091c6b827c6ee62b
techbabette <57938200+techbabette@users.noreply.github.com>
2023-10-17 10:13:47 +0200

    Add basic seeding script

---
+++ Backend NodeJS/database-seed/seed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hello, World!"
+
+mongoimport --collection users --file users.json --jsonArray --uri "mongodb://root:root@mongodb:27018/?authMechanism=SCRAM-SHA-256" --authenticationDatabase admin
+
+echo ("(+) Successfully seeded database")
